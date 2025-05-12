91027de48b07c7f8a45023ae830bb71ebc0a5f37
seal224 <jonas.granzow.hd@gmx.de>
2025-05-12 13:36:53 +0200

    remove ""
    when copying a file path from right click, it is enclosed in quotes

--- pathconverter.py
+++ pathconverter.py
@@ -2,6 +2,7 @@ import pyperclip
 path = input("Enter file path: ")
 path = path.replace("\\", "/")
 path = path.replace(":", "", 1) #replace drive letters
+path = path.replace('"', '')
 path = path.lower()
 path = "/mnt/" + path
 print(path)
